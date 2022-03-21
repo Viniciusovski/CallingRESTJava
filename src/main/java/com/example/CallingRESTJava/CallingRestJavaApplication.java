8214e81e2b83424fa24272901f564be34bf1a202
Viniciusovski <vini98.2012@gmail.com>
2022-03-21 17:59:39 -0300

    Criando projeto

---
+++ src/main/java/com/example/CallingRESTJava/CallingRestJavaApplication.java
@@ -0,0 +1,16 @@
+package com.example.CallingRESTJava;
+
+//https://www.youtube.com/watch?v=-U_dDUAw_OM
+// https://www.jsonschema2pojo.org/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallingRestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallingRestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CallingRESTJava/CallingRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CallingRESTJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
